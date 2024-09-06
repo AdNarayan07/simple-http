42cf9ead98a6fbfcf6ce6aff45f61fb6c9573dcb
AdNarayan <aditya6104narayan@gmail.com>
2024-09-06 21:54:53 +0530

    working code

--- src/http/response.rs
+++ src/http/response.rs
@@ -1,17 +1,17 @@
-use std::{env::current_dir, fmt::Display, fs, io::Result};
 use super::request::{HttpRequest, Method, Version};
+use std::{env::current_dir, fmt::Display, fs, io::Result};
 use url_escape;
 
 use mime_guess;
 
 #[derive(Debug)]
-pub struct  HttpResponse {
+pub struct HttpResponse {
     pub version: Version,
     pub status: ResponseStatus,
     pub content_length: usize,
     pub content_type: String,
     pub accept_ranges: AcceptRanges,
-    pub response_body: Vec<u8>
+    pub response_body: Vec<u8>,
 }
 
 impl HttpResponse {
@@ -19,9 +19,7 @@ impl HttpResponse {
         let status_line = format!("{} {}\r\n", self.version, self.status);
         let headers = format!(
             "content-type: {}\r\ncontent-length: {}\r\n{}\r\n\r\n",
-            self.content_type,
-            self.content_length,
-            self.accept_ranges,
+            self.content_type, self.content_length, self.accept_ranges,
         );
         let mut response = Vec::new();
         response.extend_from_slice(status_line.as_bytes());
@@ -40,8 +38,10 @@ impl HttpResponse {
         let server_root_path = current_dir()?.join("serve-dir");
         let mut resource = String::new();
         url_escape::decode_to_string(request.resource.path.clone(), &mut resource);
-        resource = resource .trim_start_matches('/')
-        .trim_end_matches('/').to_string();
+        resource = resource
+            .trim_start_matches('/')
+            .trim_end_matches('/')
+            .to_string();
         let new_path = server_root_path.join(&resource);
         let method = request.method;
         let four_o_four = b"
@@ -49,66 +49,94 @@ impl HttpResponse {
         <body>
         <h1>404 NOT FOUND</h1>
         </body>
-        </html>".to_vec();
+        </html>"
+            .to_vec();
 
-        if server_root_path.canonicalize()?.components().count() <= new_path.canonicalize()?.components().count(){
         match method {
             Method::Get => {
                 if new_path.exists() {
-                    if new_path.is_file() {
-                        let content = fs::read(&new_path)?;
-                        content_length = content.len();
-                        content_type = mime_guess::from_path(new_path).first_or_octet_stream().to_string();
-                        status = ResponseStatus::OK;
-                        accept_ranges = AcceptRanges::Bytes;
-                        response_body.extend_from_slice(&content);
-                    } else if new_path.is_dir() {
-                        let prev_path = match resource.rfind('/') { // Find the last slash
-                            Some(index) => resource[..index].to_string(), // Slice up to the last slash
-                            None => "".to_string(), // Return empty if there's no slash
-                        };
-                        let mut content = format!("
+                    let server_root = server_root_path.canonicalize()?;
+                    let new_path = new_path.canonicalize()?;
+                    let allowed = new_path.starts_with(server_root);
+
+                    if allowed
+                    {
+                        if new_path.is_file() {
+                            let content = fs::read(&new_path)?;
+                            content_length = content.len();
+                            content_type = mime_guess::from_path(new_path)
+                                .first_or_octet_stream()
+                                .to_string();
+                            status = ResponseStatus::OK;
+                            accept_ranges = AcceptRanges::Bytes;
+                            response_body.extend_from_slice(&content);
+                        } else if new_path.is_dir() {
+                            let prev_path = match resource.rfind('/') {
+                                // Find the last slash
+                                Some(index) => resource[..index].to_string(), // Slice up to the last slash
+                                None => "".to_string(), // Return empty if there's no slash
+                            };
+                            let mut content = format!(
+                                "
                         <html>
                         <head><title>Directory Listing</title></head>
                         <body>
                         <h1><a href=\"../{}\"><button>Back</button></a>Directory Listing</h1>
                         <ul>
-                        ", prev_path).into_bytes();
+                        ",
+                                prev_path
+                            )
+                            .into_bytes();
 
-                        if let Ok(entries) = fs::read_dir(new_path) {
-                            for entry in entries {
-                                if let Ok(entry) = entry {
-                                    let path = entry.path();
-                                    let file_name = path.file_name().unwrap_or_default().to_string_lossy();
+                            if let Ok(entries) = fs::read_dir(new_path) {
+                                for entry in entries {
+                                    if let Ok(entry) = entry {
+                                        let path = entry.path();
+                                        let file_name =
+                                            path.file_name().unwrap_or_default().to_string_lossy();
 
-                                    let resource_path = if resource.is_empty() {
-                                        file_name.to_string()
-                                    } else {
-                                        format!("/{}/{}", resource, file_name)
-                                    };
-                        
-                                    content.extend_from_slice(&format!(
-                                        r#"<li><a href="{}">{}</a></li>"#,
-                                        &resource_path,
-                                        &file_name
-                                    ).as_bytes());
-                                    
+                                        let resource_path = if resource.is_empty() {
+                                            file_name.to_string()
+                                        } else {
+                                            format!("/{}/{}", resource, file_name)
+                                        };
+
+                                        content.extend_from_slice(
+                                            &format!(
+                                                r#"<li><a href="{}">{}</a></li>"#,
+                                                &resource_path, &file_name
+                                            )
+                                            .as_bytes(),
+                                        );
+                                    }
                                 }
                             }
-                        }
-                        content.extend_from_slice(b"
+                            content.extend_from_slice(
+                                b"
                         </ul>
                         </body>
                         </html>
-                        ");
-                        content_length = content.len();
-                        status = ResponseStatus::OK;
-                        accept_ranges = AcceptRanges::Bytes;
-                        response_body.extend_from_slice(&content);
+                        ",
+                            );
+                            content_length = content.len();
+                            status = ResponseStatus::OK;
+                            accept_ranges = AcceptRanges::Bytes;
+                            response_body.extend_from_slice(&content);
+                        } else {
+                            println!("Not a file or directory");
+                            content_length = four_o_four.len();
+                            response_body.extend_from_slice(&four_o_four);
+                        }
                     } else {
-                        println!("Not a file or directory");
-                        content_length = four_o_four.len();
-                        response_body.extend_from_slice(&four_o_four);
+                        status = ResponseStatus::Forbidden;
+                        let four_o_one = b"
+                            <html>
+                            <body>
+                            <h1>403 Forbidden</h1>
+                            </body>
+                            </html>".to_vec();
+                        content_length = four_o_one.len();
+                        response_body.extend_from_slice(&four_o_one)
                     }
                 } else {
                     println!("Path \"{:?}\" does not exist", new_path);
@@ -122,18 +150,14 @@ impl HttpResponse {
                 response_body.extend_from_slice(&four_o_four)
             }
         }
-        } else {
-            status = ResponseStatus::Unauthorized;
-            let four_o_one = b"
-            <html>
-            <body>
-            <h1>401 Unauthorized</h1>
-            </body>
-            </html>".to_vec();
-            content_length = four_o_one.len();
-            response_body.extend_from_slice(&four_o_one)
-        }
-        Ok(HttpResponse { version, status, content_length, content_type, accept_ranges, response_body })
+        Ok(HttpResponse {
+            version,
+            status,
+            content_length,
+            content_type,
+            accept_ranges,
+            response_body,
+        })
     }
 }
 
@@ -141,7 +165,7 @@ impl HttpResponse {
 pub enum ResponseStatus {
     OK = 200,
     NotFound = 404,
-    Unauthorized = 401
+    Forbidden = 403,
 }
 
 impl Display for ResponseStatus {
@@ -149,7 +173,7 @@ impl Display for ResponseStatus {
         let msg = match self {
             ResponseStatus::OK => "200 OK",
             ResponseStatus::NotFound => "404 NOT FOUND",
-            ResponseStatus::Unauthorized => "401 UNAUTHORIZED"
+            ResponseStatus::Forbidden => "403 FORBIDDEN",
         };
         write!(f, "{}", msg)
     }
@@ -158,14 +182,14 @@ impl Display for ResponseStatus {
 #[derive(Debug)]
 pub enum AcceptRanges {
     Bytes,
-    None
+    None,
 }
 
 impl Display for AcceptRanges {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let msg = match self {
             AcceptRanges::Bytes => "accept-ranges: bytes",
-            AcceptRanges::None => "accept-ranges: none"
+            AcceptRanges::None => "accept-ranges: none",
         };
         write!(f, "{}", msg)
     }
